7e7fb60165bf2750f073a520a7e3f33d47761a0e
kratos <davidlopez.david@gmail.com>
2017-11-19 16:25:45 +1000

    Update profile

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/about.me.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  var data = {
-    me: {
-      name: 'Alexandra',
-      lastName: 'Masero',
-      prof: 'Community Manager'
-    },
-    work: {
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu rutrum leo. Sed nec imperdiet nulla. Curabitur sollicitudin tellus nibh, ut tincidunt purus dignissim a. Fusce efficitur urna eget elit viverra, ut cursus felis maximus. Proin vulputate interdum metus, condimentum laoreet massa luctus sit amet. Praesent quis tristique sapien. Vestibulum tortor nulla, porttitor et elit eget, commodo ultricies est. Sed rutrum luctus lacus, id porta turpis aliquet a. Cras rutrum nisl vel tellus ultricies, ut laoreet ex accumsan. Fusce libero magna, sagittis eget nulla ac, bibendum maximus tortor. Nam sed diam eu eros consequat tempor. Sed eu lobortis magna, in elementum ex.'
-    },
-    socialMedia: {
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu rutrum leo. Sed nec imperdiet nulla. Curabitur sollicitudin tellus nibh, ut tincidunt purus dignissim a. Fusce efficitur urna eget elit viverra, ut cursus felis maximus. Proin vulputate interdum metus, condimentum laoreet massa luctus sit amet. Praesent quis tristique sapien. Vestibulum tortor nulla, porttitor et elit eget, commodo ultricies est. Sed rutrum luctus lacus, id porta turpis aliquet a. Cras rutrum nisl vel tellus ultricies, ut laoreet ex accumsan. Fusce libero magna, sagittis eget nulla ac, bibendum maximus tortor. Nam sed diam eu eros consequat tempor. Sed eu lobortis magna, in elementum ex.'
-    },
-    contact: {
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu rutrum leo. Sed nec imperdiet nulla. Curabitur sollicitudin tellus nibh, ut tincidunt purus dignissim a. Fusce efficitur urna eget elit viverra, ut cursus felis maximus. Proin vulputate interdum metus, condimentum laoreet massa luctus sit amet. Praesent quis tristique sapien. Vestibulum tortor nulla, porttitor et elit eget, commodo ultricies est. Sed rutrum luctus lacus, id porta turpis aliquet a. Cras rutrum nisl vel tellus ultricies, ut laoreet ex accumsan. Fusce libero magna, sagittis eget nulla ac, bibendum maximus tortor. Nam sed diam eu eros consequat tempor. Sed eu lobortis magna, in elementum ex.'
-    }
-  }
 
   res.render('index', {
     data: data,
